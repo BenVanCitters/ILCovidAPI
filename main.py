22e87ee1c28ae83d1056692d927e83d2c23ac093
Ben <van.citters.b@gmail.com>
2022-04-05 00:22:50 -0500

    refactoring to use date delta and to try catch

--- main.py
+++ main.py
@@ -7,64 +7,76 @@ from datetime import datetime
 from datetime import timedelta
 import json
 
+#get data for every day from the date specified until now
+def reportCountiesFromAPI(day):
 
-def reportCountiesFromAPI(days):
+    while day < datetime.now():
+        try:
+            conn = http.client.HTTPSConnection("idph.illinois.gov")
+            payload = ''
+            headers = {}
+            datestr = day.strftime("%m/%d/%Y")
 
-    for day in days:
-        conn = http.client.HTTPSConnection("idph.illinois.gov")
-        payload = ''
-        headers = {}
-        datestr = datetime(2022, 3, day).strftime("%m/%d/%Y")
+            conn.request("GET", f'/DPHPublicInformation/api/COVID/GetCountyHistoricalTestResults?reportDate={datestr}', payload,
+                         headers)
+            res = conn.getresponse()
+            data = res.read()
 
-        conn.request("GET", f'/DPHPublicInformation/api/COVID/GetCountyHistoricalTestResults?reportDate={datestr}', payload,
-                     headers)
-        res = conn.getresponse()
-        data = res.read()
+            # returns JSON object as a dictionary
+            jsondata = json.loads(data.decode("utf-8"))
+            print(datestr)
+            # Iterating through the json
+            #print(jsondata['characteristics_by_county'])
+            for i in jsondata['characteristics_by_county']:
+                # print(i['CountyName'])
+                if (i['CountyName'] == 'Cook') or (i['CountyName'] == 'Chicago') :
+                    print(f'{i["CountyName"]}: {i["tested"]}\t{i["confirmed_cases"]}\t{i["deaths"]}')
+                if (i['CountyName'] == 'Illinois') :
+                    #different format for the whole state
+                    # - basically I copy paste it and the order of fields is different
+                    print(f'{i["CountyName"] }: { i["confirmed_cases"] }\t{ i["deaths"] }\t\t{ i["tested"] }')
 
-        # returns JSON object as
-        # a dictionary
-        jsondata = json.loads(data.decode("utf-8"))
-        print(datestr)
-        # Iterating through the json
-        #print(jsondata['characteristics_by_county'])
-        for i in jsondata['characteristics_by_county']:
-            # print(i['CountyName'])
-            if (i['CountyName'] == 'Cook') or (i['CountyName'] == 'Chicago') :
-                print(f'{i["CountyName"]}: {i["tested"]}\t{i["confirmed_cases"]}\t{i["deaths"]}')
-            if (i['CountyName'] == 'Illinois') :
-                #different format for the whole state
-                # - basically I copy paste it and the order of fields is different
-                print(f'{i["CountyName"] }: { i["confirmed_cases"] }\t{ i["deaths"] }\t\t{ i["tested"] }')
+        except Exception as inst:
+            print(inst)
 
-def getZipData(days):
+        day += timedelta(days=1)
 
-    for day in days:
-        conn = http.client.HTTPSConnection("idph.illinois.gov")
-        payload = ''
-        headers = {}
-        datestr = datetime(2022, 3, day).strftime("%m/%d/%Y")
+#get data for every day from the date specified until now
+def getZipData(day):
+    currentDay = day
+    # for day in days:
+    while currentDay < datetime.now():
+        jsondata = {}
+        try:
+            conn = http.client.HTTPSConnection("idph.illinois.gov")
+            payload = ''
+            headers = {}
+            datestr = currentDay.strftime("%m/%d/%Y")
 
-        conn.request("GET", f'/DPHPublicInformation/api/COVID/GetZip?reportDate={datestr}', payload,
-                     headers)
-        res = conn.getresponse()
-        data = res.read()
+            conn.request("GET", f'/DPHPublicInformation/api/COVID/GetZip?reportDate={datestr}', payload,
+                         headers)
+            res = conn.getresponse()
+            data = res.read()
+            # returns JSON object as
+            # a dictionary
+            jsondata = json.loads(data.decode("utf-8"))
 
-        # returns JSON object as
-        # a dictionary
-        jsondata = json.loads(data.decode("utf-8"))
+            print(datestr)
+        except Exception as inst:
+            print(inst)
 
-        print(datestr)
         # Iterating through the json
         # print(jsondata['characteristics_by_county'])
         for i in jsondata['zip_values']:
             # print(i['CountyName'])
             if (i['zip'] == '60622'):
                 print(f'{i["zip"]}: {i["total_tested"]}\t{i["confirmed_cases"]}')
+        currentDay += timedelta(days=1)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    days = range(25,26)
-    reportCountiesFromAPI(days)
-    getZipData(days)
+    startDay = datetime(2022, 3, 25)
+    reportCountiesFromAPI(startDay)
+    getZipData(startDay)
 
